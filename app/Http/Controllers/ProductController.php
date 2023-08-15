6e40a12e0ed07673b8b3e7adb7dc69dd1bb393c3
cloudy <collorucha@gmail.com>
2023-08-15 22:07:12 +0300

    crete prices and stock model for new products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductStoreRequest;
+use App\Models\Price;
 use App\Models\product;
+use App\Models\stock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class ProductController extends Controller
@@ -14,7 +17,20 @@ class ProductController extends Controller
         return view('products', ["products"=>$products]);
     }
     function store(ProductStoreRequest $request){
-        product::create($request->validated());
+        $product = new Product();
+        $product->name = $request->validated("name");
+        $product->description = $request->validated("description");
+        $product->save();
+        Price::create([
+            "product_id"=> $product->id,
+            "sale_price" => 0,
+            "bp" => 0,
+        ]);
+        stock::create([
+            "product_id" => $product->id,
+            "retail_price" => 0,
+            "qty" => 0,
+        ]);
         return redirect()->action([ProductController::class, "index"]);
     }
     function update(ProductStoreRequest $request, Product $id){
