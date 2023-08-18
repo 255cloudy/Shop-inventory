d29c248d24a7ace7544b7df9b2181921264c51a0
cloudy <collorucha@gmail.com>
2023-08-18 21:57:12 +0300

    Logo Updated

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -3,7 +3,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ route("dashboard") }}" class="brand-link">
-            <img src="{{  asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{  asset('img/B.svg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">BUDDIES</span>
         </a>
 
