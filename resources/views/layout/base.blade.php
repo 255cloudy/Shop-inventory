3f35fcbedb1b82e82aff4b529fb77ef4b582440d
cloudy <collorucha@gmail.com>
2023-08-18 08:55:17 +0300

    buddies title logo added

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Buddies</title>
+    <link rel = "icon" href =
+        "{{ asset('img/B.svg') }}"
+          type = "image/x-icon">
     <link rel="stylesheet" href="{{asset("css/app.css")}}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
