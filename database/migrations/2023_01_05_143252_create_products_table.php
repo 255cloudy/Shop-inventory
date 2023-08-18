37c5a67df2b10c80b12a3d0da686beb3998a7c75
cloudy <collorucha@gmail.com>
2023-08-18 22:06:18 +0300

    length of name field increased

--- database/migrations/2023_01_05_143252_create_products_table.php
+++ database/migrations/2023_01_05_143252_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string("name", 20);
+            $table->string("name", 60);
             $table->string("description", 200);
             $table->timestamps();
         });
