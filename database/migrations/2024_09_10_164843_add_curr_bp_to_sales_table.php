a4e5827ce66f1d5caa0f1e501c0da9acf6c66050
cloudy <collorucha@gmail.com>
2024-09-10 16:54:28 +0300

    added columns to sales table

---
+++ database/migrations/2024_09_10_164843_add_curr_bp_to_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->float(("curr_bp"));
+            $table->flaot("profit");
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->drop_column("curr_bp");
+            $table->drop_column("profit");
+        });
+    }
+};
