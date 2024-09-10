eea5814e672ffe00361e8ab523986333ac8c0af2
cloudy <collorucha@gmail.com>
2024-09-10 11:13:50 +0300

    remove modal in users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,18 +2,15 @@
 @section('main-content')
     @extends('layout.base')
     @section('main-content')
-        <!-- update  Modal -->
-        <div class="modal fade" id="product-update-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="update_modal_title">Create User</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form id="product-update-form" method="POST" >
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Create new user</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+            <form id="product-update-form" method="POST" >
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
@@ -35,14 +32,11 @@
                                 </div>
                             </div>
                         </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        
-                    </div>
-                </div>
             </div>
+            <!-- /.card-body -->
         </div>
+    </div>
+</div>
     @endsection
     @section("extra-css")
         <style>
