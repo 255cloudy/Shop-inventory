38e13ebe229e0e3c1cbff0b91c7a3665092b0597
cloudy <collorucha@gmail.com>
2024-09-18 12:38:14 +0300

    trying out sum function

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -290,7 +290,7 @@ class ReportController extends Controller
                         )
                     ->whereDate("updated_at", $request->validated("from"))
                     -> groupBy("product_id");
-        $data = $query->get();
+        // $data = $query->get();
         $query = DB::table("sales")->whereDate("updated_at", $request->validated("from"));
         dd($query->sum("total"));
         $product_data = [];
