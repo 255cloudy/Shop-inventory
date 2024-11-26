9a310a21734fb3d3f5c403645348c3b58fbe8000
cloudy <collorucha@gmail.com>
2024-11-26 21:44:57 +0300

    where between

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -28,7 +28,7 @@ class SaleController extends Controller
             ->orderBy("updated_at", "asc")
             ->get();
         return view('all_sales', ["sales"=> $sales_up,
-            "today"=> "Date:" . $today->toDateString(),
+            "today"=> "Date:" . $todayStart->toDateString(),
             "from"=> Carbon::now()->toDateString(),
             "to" => Carbon::now()->toDateString()
         ]);
