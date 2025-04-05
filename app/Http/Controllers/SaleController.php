37c0e9bb96d208a78337f39977a3406af8dd8baf
cloudy <collorucha@gmail.com>
2025-04-05 15:35:17 +0300

    dd sales

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -75,6 +75,7 @@ class SaleController extends Controller
                 $sale->curr_bp =$bp;
                 $sale->total = $qty * $price;
                 $sale->profit = ($price - $bp)*$qty;
+                dd($sale);
                 $sale->save();
             }
             $this->update_stock($entries);
